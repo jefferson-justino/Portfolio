6fce49ca00e990e72f2bf0cff872bf370da4f7c7
jefferson-justino <itachisucan@gmail.com>
2023-08-13 18:42:10 -0300

    adição de projeto
    foi adicionado mais um projeto localmente

--- controlador.js
+++ controlador.js
@@ -1,15 +1,21 @@
 let p1 = new projeto('previsao-do-tempo.png',
                     'Previsão do Tempo',
-                    'Site de previsão do tempo, onde basta pesquisar o nome da cidade e informar a sigla do estado que o site através de uma api informa a previsão',
+                    'Site de previsão do tempo, onde basta pesquisar o nome da cidade e informar a sigla do estado que o site através de uma api informa a previsão.',
                     'https://previsoes.netlify.app/')
+let p2 = new projeto('calculador-de-mercadoria.png',
+                    'Calculador de Mercadoria',
+                    'Site criado com o objetivo de solucionar um problema no cálculo de mercadoria, onde o comerciante digita o peso da mercadoria em gramas(informado pela balança) e o valor por KG, o programa informa o valor total que o cliente deve pagar.',
+                    'https://calcularmercadoria.netlify.app/')
+                        
+
 let todosProjetos =[]
-todosProjetos.push(p1)
+todosProjetos.push(p1,p2)
 
 let resultados = document.getElementById('resultados')
 function mostrar (){
-
+    resultados.innerHTML=''
 for(let i=0;i<todosProjetos.length;i++){
-resultados.innerHTML=`
+resultados.innerHTML+=`
 <a  href='${todosProjetos[i].link}' target='_blank' class='linkCard'>
 <div class='card'> 
 <img src="./img_projetos/${todosProjetos[i].foto}" alt="Previsão do Tempo" class='imagemCard'>
