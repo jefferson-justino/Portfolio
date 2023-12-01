adbc92630a93045c776ce5b334307415317cbb4c
jefferson-justino <115052745+jefferson-justino@users.noreply.github.com>
2023-12-01 13:17:56 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import myphoto from './images/myphoto.jpg'
-
+import logo from './logo.svg';
 import './App.css';
 
-import github from './images/github.png'
-
-import linkedin from './images/linkedin.png'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
