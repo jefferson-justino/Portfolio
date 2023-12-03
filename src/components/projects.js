018c7b74f560cd284d6538f369a3ccf5ba02dfd7
jefferson-justino <115052745+jefferson-justino@users.noreply.github.com>
2023-12-03 12:55:50 -0300

    responsividade

--- src/components/projects.js
+++ src/components/projects.js
@@ -4,13 +4,13 @@ import imgSorteados from '../images/projetos/sorteados.png'
 export default function Project (){
 const projects =[{
     titulo:'Sorteados',
-    descricao: 'Site de sorteio onde pode sortear números aleatórios (perfeito para rifas onde são vendidas fichas) e pode sortear nome de pessoas, basta digitar o nome dos participantes e o sistema irá sortear um aleatóriamente.',
+    descricao: 'Site de sorteio onde pode sortear números aleatórios e lista com nome de pessoas.',
     image:imgSorteados,
     link: 'https://sorteados.netlify.app/'
 },
 {
     titulo:'Calcular Mercadoria',
-    descricao: 'Site criado com o objetivo de solucionar um problema no cálculo de mercadoria, onde o comerciante digita o peso da mercadoria em gramas(informado pela balança) e o valor por KG, o programa informa o valor total que o cliente deve pagar.',
+    descricao: 'Site para realizar o cálculo do valor por KG.',
     image:imgCalculador,
     link: 'https://calcularmercadoria.netlify.app/'
 }]
